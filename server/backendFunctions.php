a6b1eec7b5fb3c7316a52c4c10938b9e9773b5f8
elynn-usu <a02391851@usu.edu>
2023-10-04 15:32:03 -0600

    improving grammar prompt

--- server/backendFunctions.php
+++ server/backendFunctions.php
@@ -4,16 +4,20 @@ include "./openaiAPI.php";
 
 function getFeedback($section, $input, $feedbackType) {
 
+    $grammarPrompt = "Provide specific feedback specifically detailing any ways the student could improve their writing's
+    spelling, punctuation, and grammar. If there are not grammatical errors, provide no feedback. Do not make up errors to
+    that do not exist. ";
+
     //format into prompt
     switch ($section) {
         case "intro":
-            $prompt = getIntroPrompt($input, $feedbackType);
+            $prompt = getIntroPrompt($input, $feedbackType, $grammarPrompt);
             break;
         case "body":
-            $prompt = getBodyPrompt($input, $feedbackType);
+            $prompt = getBodyPrompt($input, $feedbackType, $grammarPrompt);
             break;
         case "conclusion":
-            $prompt = getConclusionPrompt($input, $feedbackType);
+            $prompt = getConclusionPrompt($input, $feedbackType, $grammarPrompt);
             break;
         default:
             return "Error - invalid input";
@@ -23,7 +27,7 @@ function getFeedback($section, $input, $feedbackType) {
     return requestCompletion($prompt, 400, .3);
 }
 
-function getIntroPrompt($writing, $feedbackType) {
+function getIntroPrompt($writing, $feedbackType, $grammarPrompt) {
 
     $prompt = "You are a writing coach. You will be provided with the introduction to a college-level argumentative
     research paper. This is just the introduction to the paper, not the whole essay- do not provide the feedback
@@ -38,8 +42,7 @@ function getIntroPrompt($writing, $feedbackType) {
         $prompt = $prompt."In addition to those at least 3 pieces of feedback, ";
     }
     if (str_contains($feedbackType, "grammatical")) {
-        $prompt = $prompt."Provide specific feedback in which you detail any specific ways the student could improve the
-    introduction grammatically, including spelling, punctuation, and sentence structure. ";
+        $prompt = $prompt.$grammarPrompt;
     }
 
     return $prompt."Do not generate any large part of the actual writing, but you must provide examples of the feedback you provide.
@@ -47,7 +50,7 @@ function getIntroPrompt($writing, $feedbackType) {
     should. ".getResponseFormat()." Do not use overly technical jargon. Here is the introduction: " . $writing;
 }
 
-function getBodyPrompt($writing, $feedbackType) {
+function getBodyPrompt($writing, $feedbackType, $grammarPrompt) {
     $prompt = "You are a writing coach. You will be provided with the body paragraphs of a college-level argumentative
     research paper. ";
     if (str_contains($feedbackType, 'standards')) {
@@ -59,8 +62,7 @@ function getBodyPrompt($writing, $feedbackType) {
         $prompt = $prompt."In addition to those at least 3 pieces of feedback, ";
     }
     if (str_contains($feedbackType, "grammatical")) {
-        $prompt = $prompt."Provide specific feedback in which you detail at least 3 specific ways the student could improve the
-    body of the essay grammatically, including spelling, punctuation, and sentence structure. ";
+        $prompt = $prompt.$grammarPrompt;
     }
 
     return $prompt."Do not generate any large part of the actual writing, but you must provide examples of the feedback
@@ -68,7 +70,7 @@ function getBodyPrompt($writing, $feedbackType) {
     should. ".getResponseFormat()." Do not use overly technical jargon. Here is the body of the paper: " . $writing;
 }
 
-function getConclusionPrompt($writing, $feedbackType) {
+function getConclusionPrompt($writing, $feedbackType, $grammarPrompt) {
     $prompt = "You are a writing coach. You will be provided with the conclusion to a college-level argumentative
     research paper. This is just the conclusion to the paper, not the whole paper- do not provide the feedback
     that the conclusion should include excessive amounts of detail or evidence, like would be needed in the body
@@ -82,8 +84,7 @@ function getConclusionPrompt($writing, $feedbackType) {
         $prompt = $prompt."In addition to those at least 3 pieces of feedback, ";
     }
     if (str_contains($feedbackType, "grammatical")) {
-        $prompt = $prompt."Provide specific feedback in which you detail at least 3 specific ways the student could improve the
-    conclusion grammatically, including spelling, punctuation, and sentence structure. ";
+        $prompt = $prompt.$grammarPrompt;
     }
 
     return $prompt."Do not generate any large part of the actual writing, but you must provide examples of the feedback you provide. Remember that you provide advice and suggestions- use words like could or consider instead of
